677d2beb539de50ab01c836869c3233c9b5f4e92
Erika <Erika@LAPTOP-2EQ5SS9S>
2024-11-02 17:45:43 -0400

    calculator will carry for the next three program. This is the beginning of the program.

--- 1.4 repetition.cpp
+++ 1.4 repetition.cpp
@@ -1,20 +1,66 @@
-// 1.4 repetition.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// 1.4 repetition.cpp : This program will show the use of a calculator by creating one you need to include the operator such as addition, division, subtraction and multiplication. 
+// 
 //
 
 #include <iostream>
+#include <string>
 
+using namespace std; 
+    
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+   int number_one = 0;
+
+  int number_two = 0;
+
+  string operation;
+
+
+ cout << "Please type the name of the operator to use the calculator." << endl; 
+
+ cout << "Addition" << endl;
+ cout << "Subtraction" << endl;
+ cout << "Multiplication" << endl; 
+ cout << "Division \n " << endl; 
+
+ cout << "Enter your choice" << endl;
+
+ cin >> operation; 
+
+ cout << "Enter the first number" << endl;
+
+ cin >> number_one;
+
+ cout << "Enter the second number" << endl; 
+
+ cin >> number_two; 
+
+ if (operation == "Addition") {
+
+     cout << "The answer is: \n" << number_one + number_two << endl;
+ }
+
+ else if (operation == "Subtraction") {
+
+     cout << "The answer is: \n" << number_one - number_two << endl;
+ }
+
+ else if (operation == "Multiplication") {
+
+     cout << "The answer is: \n" << number_one * number_two << endl;
+ }
+
+ else if (operation == "Division") {
+
+     cout << "The answer is: \n" << number_one / number_two << endl;
+ }
+if (number_two== 0) {
+
+    cout << "Error: Cannot be divided by 0 \n";
+
+ }
+
+
+    return 0; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
