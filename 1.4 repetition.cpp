031e19926a121694ef7e892337f3bcf03f8221e6
Erika <Erika@LAPTOP-2EQ5SS9S>
2024-11-04 17:11:10 -0500

    fixing the output

--- 1.4 repetition.cpp
+++ 1.4 repetition.cpp
@@ -10,23 +10,26 @@ using namespace std;
 //function of a code 
 int main() {
 	//double to show the answer in a decimal point 
-	double number_one = (0, 0);
-	double number_two = (0, 0);
-	string operation;
+	double number_one = 0.0;
+	double number_two = 0.0;
+	char operation;
 	//asking user to choose an operator 
 	cout << "Please select an operator listed below:";
 	//a list of operator such as addition subtraction multiplication and divicion
-	cout << "Addition \n" "Subtraction \n" "Multiplication\n" "Division\n" << endl;
-	//where the name of the operation will be stored 
-	cin >> operation >>
+	cout << "Addition (+) \n"
+		<< "Subtraction (-) \n"
+		<< "Multiplication (*) \n"
+		<< "Division (/) \n";
+//where the name of the operation will be stored 
+	cin >> operation;
 		//asking user to enter the first number
 		cout << "Enter the first number\n";
 	//where the first number will be stored 
-		cin >> number_one >> 
+		cin >> number_one;
 			//asking user to enter the second number
 		cout << "Enter the second number\n";
 		//where the second number will be stored
-	cin >> number_two >>
+		cin >> number_two ;
 		//using the swtich operation, replacing the nested statement
 		switch (operation) {
 			//the first case is addition
@@ -48,7 +51,7 @@ int main() {
 
 			break;
 
-		case '/'
+		case '/':
 			//it will display number one and number two is equal to the answer
 
 			if (number_two != 0) {
