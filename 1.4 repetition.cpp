701ef88a67a34a855d28b800451b258f5e43bfc0
Erika <Erika@LAPTOP-2EQ5SS9S>
2024-11-04 15:22:44 -0500

    creating a calculator using switch statement.

--- 1.4 repetition.cpp
+++ 1.4 repetition.cpp
@@ -3,9 +3,69 @@
 // 
 //
 
+//including the string to allow the characters to be identified 
 #include <iostream>
 #include <string>
+using namespace std;
+//function of a code 
+int main() {
+	//double to show the answer in a decimal point 
+	double number_one = (0, 0);
+	double number_two = (0, 0);
+	string operation;
+	//asking user to choose an operator 
+	cout << "Please select an operator listed below:";
+	//a list of operator such as addition subtraction multiplication and divicion
+	cout << "Addition \n" "Subtraction \n" "Multiplication\n" "Division\n" << endl;
+	//where the name of the operation will be stored 
+	cin >> operation >>
+		//asking user to enter the first number
+		cout << "Enter the first number\n";
+	//where the first number will be stored 
+		cin >> number_one >> 
+			//asking user to enter the second number
+		cout << "Enter the second number\n";
+		//where the second number will be stored
+	cin >> number_two >>
+		//using the swtich operation, replacing the nested statement
+		switch (operation) {
+			//the first case is addition
+		case '+':
+			//it will display number one and number two is equal to the answer
+			cout << number_one << "+" << number_two << "=" << number_one + number_two << endl;
+
+			break;
+
+		case '-':
+			//it will display number one and number two is equal to the answer
+			cout << number_one << "-" << number_two << "=" << number_one - number_two << endl;
+
+			break;
+
+		case '*':
+			//it will display number one and number two is equal to the answer
+			cout << number_one << "*" << number_two << "=" << number_one * number_two << endl;
+
+			break;
+
+		case '/'
+			//it will display number one and number two is equal to the answer
+
+			if (number_two != 0) {
+
+				cout << number_one << "/" << number_two << "=" << number_one / number_two << endl;
+			}
+		//if the user enters 0 for the second number, it will display invalid: Division by zero. 
+			else
+			{
+
+				cout << "invalid: Division by zero! \n" << endl;
+			}
+			break;
 
 
-int main () 
+		}
+
+
+
 
